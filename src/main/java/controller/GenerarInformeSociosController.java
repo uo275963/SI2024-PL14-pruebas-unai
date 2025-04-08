a50040caed0933c94e889950901d715c6b68dbfe
miguelov02 <mioqui2002@gmail.com>
2025-04-08 19:42:59 +0200

    Añadida la funcionalidad de escoger el tipo de periodo de tiempo

--- src/main/java/controller/GenerarInformeSociosController.java
+++ src/main/java/controller/GenerarInformeSociosController.java
@@ -8,6 +8,13 @@ public class GenerarInformeSociosController {
 	private GenerarInformeSociosModel model;
 	private GenerarInformeSociosView view;
 	
+	// Variables globales para guardar las fechas
+		private String fechaMes;
+		private String fechaEstacion;
+		private String fechaAño;
+		private String fechaInicio;
+		private String fechaFinal;
+	
 	
 	public GenerarInformeSociosController(GenerarInformeSociosModel model, GenerarInformeSociosView view) {
 		super();
@@ -22,8 +29,90 @@ public class GenerarInformeSociosController {
 	
 	public void initController() {
 		this.initView();
+		view.getRBMes().addActionListener(e -> escogerPeriodo());
+		view.getRBEstaciones().addActionListener(e -> escogerPeriodo());
+		view.getRBAño().addActionListener(e -> escogerPeriodo());
+		view.getRBPeriodoPersonalizado().addActionListener(e -> escogerPeriodo());
 		
 	}
+	
+	public void escogerPeriodo() {
+		// Desactiva todo primero por claridad
+	    view.getCBEstaciones().setEnabled(false);
+	    view.getCBEstacionesAño().setEnabled(false);
+	    view.getCBMesInicio().setEnabled(false);
+	    view.getCBAñoInicio().setEnabled(false);
+	    view.getCBMesFinal().setEnabled(false);
+	    view.getCBAñoFinal().setEnabled(false);
+	    view.getCBAño().setEnabled(false);
+
+	    // Activar solo los necesarios según el radio seleccionado
+	    if (view.getRBMes().isSelected()) {
+	        // Supongamos que hay un CBMes, si no hay, deberías crearlo como atributo
+	        // view.getCBMes().setEnabled(true); // Este no es accesible directamente aún
+	        // Activar año si se necesita
+	        view.getCBAño().setEnabled(true);
+	    } else if (view.getRBEstaciones().isSelected()) {
+	        view.getCBEstaciones().setEnabled(true);
+	        view.getCBEstacionesAño().setEnabled(true);
+	    } else if (view.getRBAño().isSelected()) {
+	        view.getCBAño().setEnabled(true);
+	    } else if (view.getRBPeriodoPersonalizado().isSelected()) {
+	        view.getCBMesInicio().setEnabled(true);
+	        view.getCBAñoInicio().setEnabled(true);
+	        view.getCBMesFinal().setEnabled(true);
+	        view.getCBAñoFinal().setEnabled(true);
+	    }
+		
+		
+	}
+	
+	
+	public void obtenerFechasComoString() {
+	    // Limpia las anteriores por si se cambia de opción
+	    fechaMes = null;
+	    fechaEstacion = null;
+	    fechaAño = null;
+	    fechaInicio = null;
+	    fechaFinal = null;
+
+	    if (view.getRBMes().isSelected()) {
+	        int mes = view.getCBMes().getSelectedIndex() + 1;
+	        String año = view.getCBAño().getSelectedItem().toString();
+	        fechaMes = año + "-" + String.format("%02d", mes);
+	        System.out.println("Fecha Mes: " + fechaMes);
+
+	    } else if (view.getRBEstaciones().isSelected()) {
+	        String año = view.getCBEstacionesAño().getSelectedItem().toString();
+	        String estacion = view.getCBEstaciones().getSelectedItem().toString();
+	        fechaEstacion = año + "-" + estacion;
+	        System.out.println("Fecha Estación: " + fechaEstacion);
+
+	    } else if (view.getRBAño().isSelected()) {
+	        fechaAño = view.getCBAño().getSelectedItem().toString();
+	        System.out.println("Fecha Año: " + fechaAño);
+
+	    } else if (view.getRBPeriodoPersonalizado().isSelected()) {
+	        int mesInicio = view.getCBMesInicio().getSelectedIndex() + 1;
+	        String añoInicio = view.getCBAñoInicio().getSelectedItem().toString();
+	        fechaInicio = añoInicio + "-" + String.format("%02d", mesInicio);
+
+	        int mesFinal = view.getCBMesFinal().getSelectedIndex() + 1;
+	        String añoFinal = view.getCBAñoFinal().getSelectedItem().toString();
+	        fechaFinal = añoFinal + "-" + String.format("%02d", mesFinal);
+
+	        System.out.println("Fecha Desde: " + fechaInicio);
+	        System.out.println("Fecha Hasta: " + fechaFinal);
+	    }
+	}
+	
+	
+	
+	public void GenerarInforme() {
+		
+	}
+
+
 	
 	
 	
--- src/main/java/view/GenerarInformeSociosView.java
+++ src/main/java/view/GenerarInformeSociosView.java
@@ -3,10 +3,30 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 
 public class GenerarInformeSociosView {
 
 	private JFrame frame;
+	private JRadioButton RBMes;
+	private JRadioButton RBEstaciones;
+	private JRadioButton RBAño;
+	private JRadioButton RBPeriodoPersonalizado;
+	private JComboBox CBEstaciones;
+	private JComboBox CBEstacionesAño;
+	private JComboBox CBAño;
+	private JComboBox CBMesInicio;
+	private JComboBox CBAñoInicio;
+	private JComboBox CBMesFinal;
+	private JComboBox CBAñoFinal;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JComboBox CBMes;
 
 	/**
 	 * Launch the application.
@@ -47,6 +67,219 @@ public class GenerarInformeSociosView {
 		frame.setTitle("InformeSocios");
 		frame.setBounds(100, 100, 789, 547);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Escoga un periodo de tiempo:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel.setBounds(10, 10, 192, 29);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Estaciones:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(10, 120, 103, 13);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		CBEstaciones = new JComboBox();
+		CBEstaciones.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		CBEstaciones.setModel(new DefaultComboBoxModel(new String[] {"Primavera", "Verano", "Otoño", "Invierno"}));
+		CBEstaciones.setBounds(10, 140, 100, 30);
+		frame.getContentPane().add(CBEstaciones);
+		
+		CBEstacionesAño = new JComboBox();
+		CBEstacionesAño.setBounds(120, 140, 100, 30);
+		frame.getContentPane().add(CBEstacionesAño);
+		
+		RBEstaciones = new JRadioButton("Estaciones");
+		RBEstaciones.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		buttonGroup.add(RBEstaciones);
+		RBEstaciones.setBounds(302, 15, 92, 21);
+		frame.getContentPane().add(RBEstaciones);
+		
+		RBPeriodoPersonalizado = new JRadioButton("Periodo personalizado");
+		RBPeriodoPersonalizado.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		buttonGroup.add(RBPeriodoPersonalizado);
+		RBPeriodoPersonalizado.setBounds(494, 14, 163, 21);
+		frame.getContentPane().add(RBPeriodoPersonalizado);
+		
+		RBMes = new JRadioButton("Mes");
+		RBMes.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		buttonGroup.add(RBMes);
+		RBMes.setBounds(208, 15, 70, 21);
+		frame.getContentPane().add(RBMes);
+		
+		RBAño = new JRadioButton("Año");
+		RBAño.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		buttonGroup.add(RBAño);
+		RBAño.setBounds(407, 15, 85, 21);
+		frame.getContentPane().add(RBAño);
+		
+		JLabel lblNewLabel_2 = new JLabel("Mes");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(10, 50, 45, 13);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		CBMes = new JComboBox();
+		CBMes.setBounds(10, 70, 100, 30);
+		frame.getContentPane().add(CBMes);
+		
+		JLabel lblNewLabel_3 = new JLabel("Año");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_3.setBounds(10, 190, 45, 13);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		CBAño = new JComboBox();
+		CBAño.setBounds(10, 210, 100, 30);
+		frame.getContentPane().add(CBAño);
+		
+		JLabel lblNewLabel_4 = new JLabel("Personalizado");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_4.setBounds(10, 260, 85, 13);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Desde");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_5.setBounds(10, 288, 45, 13);
+		frame.getContentPane().add(lblNewLabel_5);
+		
+		CBMesInicio = new JComboBox();
+		CBMesInicio.setBounds(66, 280, 100, 30);
+		frame.getContentPane().add(CBMesInicio);
+		
+		CBAñoInicio = new JComboBox();
+		CBAñoInicio.setBounds(176, 280, 100, 30);
+		frame.getContentPane().add(CBAñoInicio);
+		
+		JLabel lblNewLabel_6 = new JLabel("hasta");
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_6.setBounds(292, 288, 45, 13);
+		frame.getContentPane().add(lblNewLabel_6);
+		
+		CBMesFinal = new JComboBox();
+		CBMesFinal.setBounds(347, 280, 100, 30);
+		frame.getContentPane().add(CBMesFinal);
+		
+		CBAñoFinal = new JComboBox();
+		CBAñoFinal.setBounds(457, 280, 100, 30);
+		frame.getContentPane().add(CBAñoFinal);
+		
+		JButton btnGenerarInforme = new JButton("Generar informe");
+		btnGenerarInforme.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnGenerarInforme.setBounds(146, 470, 150, 30);
+		frame.getContentPane().add(btnGenerarInforme);
+		
+		JButton btnCerrar = new JButton("Cerrar");
+		btnCerrar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnCerrar.setBounds(27, 470, 100, 30);
+		frame.getContentPane().add(btnCerrar);
+	}
+	
+	
+	
+	
+
+
+	public JComboBox getCBMes() {
+		return CBMes;
+	}
+
+	public void setCBMes(JComboBox cBMes) {
+		CBMes = cBMes;
+	}
+
+	public JRadioButton getRBMes() {
+		return RBMes;
+	}
+
+	public void setRBMes(JRadioButton rBMes) {
+		RBMes = rBMes;
+	}
+
+	public JRadioButton getRBEstaciones() {
+		return RBEstaciones;
+	}
+
+	public void setRBEstaciones(JRadioButton rBEstaciones) {
+		RBEstaciones = rBEstaciones;
+	}
+
+	public JRadioButton getRBAño() {
+		return RBAño;
+	}
+
+	public void setRBAño(JRadioButton rBAño) {
+		RBAño = rBAño;
+	}
+
+	public JRadioButton getRBPeriodoPersonalizado() {
+		return RBPeriodoPersonalizado;
+	}
+
+	public void setRBPeriodoPersonalizado(JRadioButton rBPeriodoPersonalizado) {
+		RBPeriodoPersonalizado = rBPeriodoPersonalizado;
+	}
+
+	public ButtonGroup getButtonGroup() {
+		return buttonGroup;
+	}
+
+	public JComboBox getCBEstaciones() {
+		return CBEstaciones;
+	}
+
+	public void setCBEstaciones(JComboBox cBEstaciones) {
+		CBEstaciones = cBEstaciones;
+	}
+
+	public JComboBox getCBEstacionesAño() {
+		return CBEstacionesAño;
+	}
+
+	public void setCBEstacionesAño(JComboBox cBEstacionesAño) {
+		CBEstacionesAño = cBEstacionesAño;
+	}
+
+	public JComboBox getCBAño() {
+		return CBAño;
+	}
+
+	public void setCBAño(JComboBox cBAño) {
+		CBAño = cBAño;
+	}
+
+	public JComboBox getCBMesInicio() {
+		return CBMesInicio;
+	}
+
+	public void setCBMesInicio(JComboBox cBMesInicio) {
+		CBMesInicio = cBMesInicio;
+	}
+
+	public JComboBox getCBAñoInicio() {
+		return CBAñoInicio;
+	}
+
+	public void setCBAñoInicio(JComboBox cBAñoInicio) {
+		CBAñoInicio = cBAñoInicio;
+	}
+
+	public JComboBox getCBMesFinal() {
+		return CBMesFinal;
+	}
+
+	public void setCBMesFinal(JComboBox cBMesFinal) {
+		CBMesFinal = cBMesFinal;
+	}
+
+	public JComboBox getCBAñoFinal() {
+		return CBAñoFinal;
+	}
+
+	public void setCBAñoFinal(JComboBox cBAñoFinal) {
+		CBAñoFinal = cBAñoFinal;
+	}
+
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
 	}
 
 	public JFrame getFrame() {
@@ -56,5 +289,4 @@ public class GenerarInformeSociosView {
 	public void setFrmInformesocios(JFrame frmInformesocios) {
 		this.frame = frmInformesocios;
 	}
-
 }
