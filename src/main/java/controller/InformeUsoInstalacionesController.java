c6e1c8c1491e2859b2ab1ed342596327997a85cd
ivanp <ivanp@DESKTOP-19M49VK>
2025-04-11 12:18:07 +0200

    Primeras aportaciones a la HU:
    -Se actulaizan los combobox

--- src/main/java/controller/InformeUsoInstalacionesController.java
+++ src/main/java/controller/InformeUsoInstalacionesController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javax.swing.DefaultComboBoxModel;
+
 import model.InformeUsoInstalacionesModel;
 import view.InformeUsoInstalacionesView;
 
@@ -17,7 +19,67 @@ public class InformeUsoInstalacionesController {
 		view.getFrame().setVisible(true);
 	}
 	public void initController() {
+		deshabilitarComponentes();
+		view.getrBMes().addActionListener(e -> escogerPeriodo());
+		view.getrBCuatrimestre().addActionListener(e -> escogerPeriodo());
+		view.getrBAño().addActionListener(e -> escogerPeriodo());
+		view.getrBPersonalizado().addActionListener(e -> escogerPeriodo());
+	}
+	
+	private void deshabilitarComponentes() {
+		view.getcBMesMes().setEnabled(false);
+		view.getcBAñoMes().setEnabled(false);
+		view.getcBCuatrimestreMes().setEnabled(false);
+		view.getcBCuatrimestreAño().setEnabled(false);
+		view.getcBAño().setEnabled(false);
+		view.getcBPersonalizadoMesInicio().setEnabled(false);
+		view.getcBPersonalizadoAñoInicio().setEnabled(false);
+		view.getcBPersonalizadoMesFin().setEnabled(false);
+		view.getcBPersonalizadoAñoFin().setEnabled(false);
+	}
+	
+	private void escogerPeriodo() {
+		deshabilitarComponentes();
+		cargarAños();
+		if(view.getrBMes().isSelected()) {
+			view.getcBMesMes().setEnabled(true);
+			view.getcBAñoMes().setEnabled(true);
+		}else if(view.getrBCuatrimestre().isSelected()){
+			view.getcBCuatrimestreMes().setEnabled(true);
+			view.getcBCuatrimestreAño().setEnabled(true);
+		}else if(view.getrBAño().isSelected()) {
+			view.getcBAño().setEnabled(true);
+		}else if(view.getrBPersonalizado().isSelected()) {
+			view.getcBPersonalizadoMesInicio().setEnabled(true);
+			view.getcBPersonalizadoAñoInicio().setEnabled(true);
+			view.getcBPersonalizadoMesFin().setEnabled(true);
+			view.getcBPersonalizadoAñoFin().setEnabled(true);
+		}
+	}
+	
+	private void cargarAños() {
+		// Lista de años
+		String[]años = {"2024","2025"};
 		
+		// Establecer el modelo para el ComboBox de años
+		DefaultComboBoxModel<String> modelo = new DefaultComboBoxModel<String>(años);
+		view.getcBAñoMes().setModel(modelo);
+		view.getcBCuatrimestreAño().setModel(modelo);
+		view.getcBAño().setModel(modelo);
+		view.getcBPersonalizadoAñoFin().setModel(modelo);
+		view.getcBPersonalizadoAñoInicio().setModel(modelo);
+	}
+	
+	private void cargarMeses() {
+		String[] meses = {"Enero","Febrero","Marzo","Abril","Mayo",
+				"Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+		
+		// Establecer el modelo para el combox de meses
+		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>(meses);
+		view.getcBMesMes().setModel(model);
+		view.getcBCuatrimestreMes().setModel(model);
+		view.getcBPersonalizadoMesInicio().setModel(model);
+		view.getcBPersonalizadoMesFin().setModel(model);
 	}
 
 }
--- src/main/java/view/InformeUsoInstalacionesView.java
+++ src/main/java/view/InformeUsoInstalacionesView.java
@@ -16,6 +16,21 @@ public class InformeUsoInstalacionesView {
 
 	private JFrame frame;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JRadioButton rBMes;
+	private JRadioButton rBCuatrimestre;
+	private JRadioButton rBAño;
+	private JRadioButton rBPersonalizado;
+	private JComboBox cBMesMes;
+	private JComboBox cBAñoMes;
+	private JComboBox cBCuatrimestreMes;
+	private JComboBox cBCuatrimestreAño;
+	private JComboBox cBAño;
+	private JComboBox cBPersonalizadoMesInicio;
+	private JComboBox cBPersonalizadoAñoInicio;
+	private JComboBox cBPersonalizadoMesFin;
+	private JComboBox cBPersonalizadoAñoFin;
+	private JButton bCerrar;
+	private JButton bInforme;
 
 	/**
 	 * Launch the application.
@@ -53,64 +68,64 @@ public class InformeUsoInstalacionesView {
 		lblNewLabel.setBounds(34, 37, 178, 33);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JRadioButton RBMes = new JRadioButton("Mes");
-		buttonGroup.add(RBMes);
-		RBMes.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		RBMes.setBounds(218, 42, 74, 26);
-		frame.getContentPane().add(RBMes);
+		rBMes = new JRadioButton("Mes");
+		buttonGroup.add(rBMes);
+		rBMes.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		rBMes.setBounds(218, 42, 74, 26);
+		frame.getContentPane().add(rBMes);
 		
-		JRadioButton RBCuatrimestre = new JRadioButton("Cuatrimestre");
-		buttonGroup.add(RBCuatrimestre);
-		RBCuatrimestre.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		RBCuatrimestre.setBounds(305, 42, 127, 26);
-		frame.getContentPane().add(RBCuatrimestre);
+		rBCuatrimestre = new JRadioButton("Cuatrimestre");
+		buttonGroup.add(rBCuatrimestre);
+		rBCuatrimestre.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		rBCuatrimestre.setBounds(305, 42, 127, 26);
+		frame.getContentPane().add(rBCuatrimestre);
 		
-		JRadioButton RBAño = new JRadioButton("Año");
-		buttonGroup.add(RBAño);
-		RBAño.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		RBAño.setBounds(455, 41, 90, 26);
-		frame.getContentPane().add(RBAño);
+		rBAño = new JRadioButton("Año");
+		buttonGroup.add(rBAño);
+		rBAño.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		rBAño.setBounds(455, 41, 90, 26);
+		frame.getContentPane().add(rBAño);
 		
-		JRadioButton RBPersonalizado = new JRadioButton("Personalizado");
-		buttonGroup.add(RBPersonalizado);
-		RBPersonalizado.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		RBPersonalizado.setBounds(547, 39, 138, 33);
-		frame.getContentPane().add(RBPersonalizado);
+		rBPersonalizado = new JRadioButton("Personalizado");
+		buttonGroup.add(rBPersonalizado);
+		rBPersonalizado.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		rBPersonalizado.setBounds(547, 39, 138, 33);
+		frame.getContentPane().add(rBPersonalizado);
 		
 		JLabel lblNewLabel_1 = new JLabel("Mes: ");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblNewLabel_1.setBounds(34, 98, 74, 26);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		JComboBox CBMesMes = new JComboBox();
-		CBMesMes.setBounds(150, 101, 90, 22);
-		frame.getContentPane().add(CBMesMes);
+		cBMesMes = new JComboBox();
+		cBMesMes.setBounds(150, 101, 90, 22);
+		frame.getContentPane().add(cBMesMes);
 		
-		JComboBox CBAñoMes = new JComboBox();
-		CBAñoMes.setBounds(276, 101, 90, 22);
-		frame.getContentPane().add(CBAñoMes);
+		cBAñoMes = new JComboBox();
+		cBAñoMes.setBounds(276, 101, 90, 22);
+		frame.getContentPane().add(cBAñoMes);
 		
 		JLabel lblNewLabel_2 = new JLabel("Cuatrimestre:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblNewLabel_2.setBounds(34, 135, 120, 22);
 		frame.getContentPane().add(lblNewLabel_2);
 		
-		JComboBox CBCuatrimestreMes = new JComboBox();
-		CBCuatrimestreMes.setBounds(150, 136, 90, 22);
-		frame.getContentPane().add(CBCuatrimestreMes);
+		cBCuatrimestreMes = new JComboBox();
+		cBCuatrimestreMes.setBounds(150, 136, 90, 22);
+		frame.getContentPane().add(cBCuatrimestreMes);
 		
-		JComboBox CBCuatrimestreAño = new JComboBox();
-		CBCuatrimestreAño.setBounds(276, 136, 90, 22);
-		frame.getContentPane().add(CBCuatrimestreAño);
+		cBCuatrimestreAño = new JComboBox();
+		cBCuatrimestreAño.setBounds(276, 136, 90, 22);
+		frame.getContentPane().add(cBCuatrimestreAño);
 		
 		JLabel lblNewLabel_3 = new JLabel("Año:");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblNewLabel_3.setBounds(34, 174, 48, 14);
 		frame.getContentPane().add(lblNewLabel_3);
 		
-		JComboBox CBAño = new JComboBox();
-		CBAño.setBounds(150, 171, 90, 22);
-		frame.getContentPane().add(CBAño);
+		cBAño = new JComboBox();
+		cBAño.setBounds(150, 171, 90, 22);
+		frame.getContentPane().add(cBAño);
 		
 		JLabel lblNewLabel_4 = new JLabel("Personalizado:");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -121,37 +136,37 @@ public class InformeUsoInstalacionesView {
 		lblNewLabel_5.setBounds(150, 211, 30, 14);
 		frame.getContentPane().add(lblNewLabel_5);
 		
-		JComboBox CBPersonalizadoMesInicio = new JComboBox();
-		CBPersonalizadoMesInicio.setBounds(181, 207, 74, 22);
-		frame.getContentPane().add(CBPersonalizadoMesInicio);
+		cBPersonalizadoMesInicio = new JComboBox();
+		cBPersonalizadoMesInicio.setBounds(181, 207, 74, 22);
+		frame.getContentPane().add(cBPersonalizadoMesInicio);
 		
-		JComboBox CBPersonalizadoAñoInicio = new JComboBox();
-		CBPersonalizadoAñoInicio.setBounds(271, 207, 74, 22);
-		frame.getContentPane().add(CBPersonalizadoAñoInicio);
+		cBPersonalizadoAñoInicio = new JComboBox();
+		cBPersonalizadoAñoInicio.setBounds(271, 207, 74, 22);
+		frame.getContentPane().add(cBPersonalizadoAñoInicio);
 		
 		JLabel lblNewLabel_6 = new JLabel("Hasta: ");
 		lblNewLabel_6.setBounds(377, 211, 48, 14);
 		frame.getContentPane().add(lblNewLabel_6);
 		
-		JComboBox CBPersonalizadoMesFin = new JComboBox();
-		CBPersonalizadoMesFin.setBounds(435, 207, 74, 22);
-		frame.getContentPane().add(CBPersonalizadoMesFin);
+		cBPersonalizadoMesFin = new JComboBox();
+		cBPersonalizadoMesFin.setBounds(435, 207, 74, 22);
+		frame.getContentPane().add(cBPersonalizadoMesFin);
 		
-		JComboBox CBPersonalizadoAñoFin = new JComboBox();
-		CBPersonalizadoAñoFin.setBounds(519, 207, 90, 22);
-		frame.getContentPane().add(CBPersonalizadoAñoFin);
+		cBPersonalizadoAñoFin = new JComboBox();
+		cBPersonalizadoAñoFin.setBounds(519, 207, 90, 22);
+		frame.getContentPane().add(cBPersonalizadoAñoFin);
 		
 		JSeparator separator = new JSeparator();
 		separator.setBounds(0, 256, 754, 2);
 		frame.getContentPane().add(separator);
 		
-		JButton BCerrar = new JButton("Cerrar");
-		BCerrar.setBounds(643, 397, 91, 23);
-		frame.getContentPane().add(BCerrar);
+		bCerrar = new JButton("Cerrar");
+		bCerrar.setBounds(643, 397, 91, 23);
+		frame.getContentPane().add(bCerrar);
 		
-		JButton BInforme = new JButton("Generar Informe");
-		BInforme.setBounds(487, 397, 146, 23);
-		frame.getContentPane().add(BInforme);
+		bInforme = new JButton("Generar Informe");
+		bInforme.setBounds(487, 397, 146, 23);
+		frame.getContentPane().add(bInforme);
 	}
 
 	public JFrame getFrame() {
@@ -161,4 +176,126 @@ public class InformeUsoInstalacionesView {
 	public void setFrame(JFrame frame) {
 		this.frame = frame;
 	}
+
+	public JRadioButton getrBMes() {
+		return rBMes;
+	}
+
+	public void setrBMes(JRadioButton rBMes) {
+		this.rBMes = rBMes;
+	}
+
+	public JRadioButton getrBCuatrimestre() {
+		return rBCuatrimestre;
+	}
+
+	public void setrBCuatrimestre(JRadioButton rBCuatrimestre) {
+		this.rBCuatrimestre = rBCuatrimestre;
+	}
+
+	public JRadioButton getrBAño() {
+		return rBAño;
+	}
+
+	public void setrBAño(JRadioButton rBAño) {
+		this.rBAño = rBAño;
+	}
+
+	public JRadioButton getrBPersonalizado() {
+		return rBPersonalizado;
+	}
+
+	public void setrBPersonalizado(JRadioButton rBPersonalizado) {
+		this.rBPersonalizado = rBPersonalizado;
+	}
+
+	public JComboBox getcBMesMes() {
+		return cBMesMes;
+	}
+
+	public void setcBMesMes(JComboBox cBMesMes) {
+		this.cBMesMes = cBMesMes;
+	}
+
+	public JComboBox getcBAñoMes() {
+		return cBAñoMes;
+	}
+
+	public void setcBAñoMes(JComboBox cBAñoMes) {
+		this.cBAñoMes = cBAñoMes;
+	}
+
+	public JComboBox getcBCuatrimestreMes() {
+		return cBCuatrimestreMes;
+	}
+
+	public void setcBCuatrimestreMes(JComboBox cBCuatrimestreMes) {
+		this.cBCuatrimestreMes = cBCuatrimestreMes;
+	}
+
+	public JComboBox getcBCuatrimestreAño() {
+		return cBCuatrimestreAño;
+	}
+
+	public void setcBCuatrimestreAño(JComboBox cBCuatrimestreAño) {
+		this.cBCuatrimestreAño = cBCuatrimestreAño;
+	}
+
+	public JComboBox getcBAño() {
+		return cBAño;
+	}
+
+	public void setcBAño(JComboBox cBAño) {
+		this.cBAño = cBAño;
+	}
+
+	public JComboBox getcBPersonalizadoMesInicio() {
+		return cBPersonalizadoMesInicio;
+	}
+
+	public void setcBPersonalizadoMesInicio(JComboBox cBPersonalizadoMesInicio) {
+		this.cBPersonalizadoMesInicio = cBPersonalizadoMesInicio;
+	}
+
+	public JComboBox getcBPersonalizadoAñoInicio() {
+		return cBPersonalizadoAñoInicio;
+	}
+
+	public void setcBPersonalizadoAñoInicio(JComboBox cBPersonalizadoAñoInicio) {
+		this.cBPersonalizadoAñoInicio = cBPersonalizadoAñoInicio;
+	}
+
+	public JComboBox getcBPersonalizadoMesFin() {
+		return cBPersonalizadoMesFin;
+	}
+
+	public void setcBPersonalizadoMesFin(JComboBox cBPersonalizadoMesFin) {
+		this.cBPersonalizadoMesFin = cBPersonalizadoMesFin;
+	}
+
+	public JComboBox getcBPersonalizadoAñoFin() {
+		return cBPersonalizadoAñoFin;
+	}
+
+	public void setcBPersonalizadoAñoFin(JComboBox cBPersonalizadoAñoFin) {
+		this.cBPersonalizadoAñoFin = cBPersonalizadoAñoFin;
+	}
+
+	public JButton getbCerrar() {
+		return bCerrar;
+	}
+
+	public void setbCerrar(JButton bCerrar) {
+		this.bCerrar = bCerrar;
+	}
+
+	public JButton getbInforme() {
+		return bInforme;
+	}
+
+	public void setbInforme(JButton bInforme) {
+		this.bInforme = bInforme;
+	}
+	
+	
 }
